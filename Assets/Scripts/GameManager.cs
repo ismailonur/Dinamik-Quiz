baf695a1565a070f1ae7efbbc4aafa2a8ac03386
ismailonur <ismailonur15@gmail.com>
2020-09-28 17:22:28 +0300

    Doğru yanlış yapıldı.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Soru[] sorular;
+    private static List<Soru> cevaplanmamisSorular;
+
+    private Soru gecerliSoru;
+
+    public Text soruText;
+
+    public Text dogruCevapText, yanlisCevapText;
+
+    void Start()
+    {
+        if(cevaplanmamisSorular == null || cevaplanmamisSorular.Count == 0)
+        {
+            cevaplanmamisSorular = sorular.ToList<Soru>();
+        }
+
+        RastgeleSoruSec();
+    }
+
+    void RastgeleSoruSec()
+    {
+        int randomSoruIndex = Random.Range(0, cevaplanmamisSorular.Count);
+        gecerliSoru = cevaplanmamisSorular[randomSoruIndex];
+
+        soruText.text = gecerliSoru.soru;
+
+        if (gecerliSoru.dogrumu)
+        {
+            dogruCevapText.text = "DOĞRU CEVAPLADINIZ.";
+            yanlisCevapText.text = "YANLIŞ CEVAPLADINIZ!";
+        }
+        else
+        {
+            dogruCevapText.text = "YANLIŞ CEVAPLADINIZ.";
+            yanlisCevapText.text = "DOĞRU CEVAPLADINIZ!";
+        }
+    }
+
+    IEnumerator SorularArasiBekle()
+    {
+        cevaplanmamisSorular.Remove(gecerliSoru);
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void DogruButonaBasildi()
+    {
+        if (gecerliSoru.dogrumu)
+        {
+            Debug.Log("Doğru");
+        }
+        else
+        {
+            Debug.Log("Yanlış");
+        }
+
+        StartCoroutine(SorularArasiBekle());
+    }
+    
+    public void YanlisButonaBasıldı()
+    {
+        if (!gecerliSoru.dogrumu)
+        {
+            Debug.Log("Doğru");
+        }
+        else
+        {
+            Debug.Log("Yanlış");
+        }
+
+        StartCoroutine(SorularArasiBekle());
+    }
+}
